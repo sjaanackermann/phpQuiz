26fa3f4c62414e6c9315bef83474b7cd2a3b9508
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-02-29 14:33:52 +0200

    index page updated

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Harry Potter Quiz</title>
 </head>
 <body>
+ <div class="homepg">
+    <h1>Welcome to the Ultimate Harry Potter Quiz!</h1>
+    <h3>Prove that you are the BIGGEST FAN by testing your knowledge.</h3>
     
+    <form action="questions.php" method="post">
+    Please enter your Name: <input type="text" name="name" required><br>
+    Please select your House: <input type="" name="" required><br>
+    <input type="submit" class="submitbtn">
+ </div>
 </body>
 </html>
